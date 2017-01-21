2998aefc13ae1245f66b0b030116048343007d02
Tim Gottschalk <Tim@MacBook-Air-2.local>
2017-01-21 18:07:38 -0500

    Added live search functionality to main page

--- GoServer/static/js/script.js
+++ GoServer/static/js/script.js
@@ -1,12 +1,8 @@
-/**
- * Created by Tim on 2017-01-21.
- */
-
-<script>
-$('#search').keyup(function(){
-    console.log("Key press registered: " + $('#search').val());
-    $.get( "/searchResult?q="+$('#search').val(), function( data ) {
-        $("#result").html(data);
+$(document).ready(function() {
+    $('#search').keyup(function () {
+        console.log("Key press registered: " + $('#search').val());
+        $.get("/searchResult?q=" + $('#search').val(), function (data) {
+            $("#result").html(data);
+        });
     });
 });
-</script>
