f3a032ea62e7b761f9ad13163dfc8ac9124f945e
Tim Gottschalk <Tim@MacBook-Air-2.local>
2017-01-21 20:05:15 -0500

    Updating search results display

--- GoServer/static/js/script.js
+++ GoServer/static/js/script.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
     $('#search').keyup(function () {
-        console.log("Key press registered: " + $('#search').val());
         $.get("/searchResult?q=" + $('#search').val(), function (data) {
             $("#result").html(data);
         });
